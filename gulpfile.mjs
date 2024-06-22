23fcbb74036d691d01ddbcaf9263ec1630f1a2d2
Moustafa Hassanin <moustafa.mhassanin@gmail.com>
2024-06-22 14:12:46 +0300

    update changes

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -2,6 +2,7 @@ import gulp from 'gulp';
 import concat from 'gulp-concat';
 import prefix from 'gulp-autoprefixer';
 import replace from 'gulp-replace';
+import uglify from 'gulp-uglify';
 import browserSync from 'browser-sync';
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
@@ -9,7 +10,7 @@ const sass = gulpSass(dartSass);
 
 //Tasks
 export const css = () => gulp
-.src('src/main.scss')
+.src('src/css/main.scss')
 .pipe(sass().on('error', sass.logError))
 .pipe(concat('test.css.liquid'))
 .pipe(prefix('last 2 version'))
@@ -18,17 +19,21 @@ export const css = () => gulp
 .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 .pipe(replace('"\{\{', '{{'))
 .pipe(replace('\}\}"', '}}'))
-.pipe(concat('a--main.min.css.liquid'))
+.pipe(concat('stylesheet.min.css.liquid'))
 .pipe(replace('/*!--',''))
 .pipe(replace('--*/',''))
 .pipe(gulp.dest('theme-files/assets'))
 .pipe(browserSync.stream());
 
 export const js = () => gulp 
-.src('scripts/main.js')
+.src('src/js/main.js')
+.pipe(uglify())
+.pipe(concat('scripts.min.js.liquid'))
+.pipe(gulp.dest('dist/js'))
+.pipe(browserSync.stream());
 
 export const fontawesome = () => gulp
-.src('src/font-awesome-pro.scss')
+.src('src/css/font-awesome-pro.scss')
 .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 .pipe(concat('a--fontawesome-all.css'))
 .pipe(replace('../webfonts/',''))
@@ -43,6 +48,7 @@ export const server = () => {
     });
     gulp.watch('src/**/*.scss', css);
     gulp.watch('src/**/*.scss', fontawesome);
+    gulp.watch('src/**/*.js', js);
 }
 
-export default gulp.series(css, fontawesome, server);
+export default gulp.series(css, js, fontawesome, server);
